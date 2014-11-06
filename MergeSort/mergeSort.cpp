c342686fe354745e3a8a97101e3d1bab5daf04e2
zhao-han <windzhaohan@gmail.com>
2014-11-06 08:49:21 +0100

    mergeSort() finished

--- MergeSort/mergeSort.cpp
+++ MergeSort/mergeSort.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 template <typename T>
-std::vector<T> mergeSort(std::vector<T> array)
+std::vector<T> mergeSort(const std::vector<T>& array)
 {
     if(array.size() == 1)
     {
@@ -11,5 +11,14 @@ std::vector<T> mergeSort(std::vector<T> array)
 
     std::vector<T>::iterator middle = array.begin() + (array.size() / 2);
 
+    std::vector<T> left(array.begin(), middle);
+    std::vector<T> right(middle, array.end());
 
+    left = mergeSort(left);
+    right = mergeSort(right);
+
+    return merge(array, left, right);
 }
+
+template <typename T>
+
